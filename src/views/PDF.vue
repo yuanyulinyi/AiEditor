a855e1349aaab1835bd78e7a67d85c48db14d1f9
yuanyulinyi <2169788850@qq.com>
2024-10-19 17:22:00 +0800

    输出框控制pdf路径

--- src/views/PDF.vue
+++ src/views/PDF.vue
@@ -1,26 +1,49 @@
 <template>
-    <vue-office-pdf :src="pdf" style="height: 100vh;margin:0,25%" @rendered="renderedHandler" @error="errorHandler" />
+    <div>
+        <el-input v-model="input" style="width: 240px" placeholder="请输入访问pdf的url" />
+        <el-button type="primary" @click="updatePdf">提交</el-button>
+    </div>
+    <vue-office-pdf :src="pdfUrl" style="height: 100vh;width: 100vh" @rendered="renderedHandler"
+        @error="errorHandler" />
 </template>
 
 <script>
-// 引入 VueOfficePdf 组件
+import { ref } from 'vue'
 import VueOfficePdf from '@vue-office/pdf'
+import { ElInput, ElButton, ElMessage } from 'element-plus'
 
 export default {
     components: {
-        VueOfficePdf
+        VueOfficePdf,
+        ElInput,
+        ElButton
     },
-    data() {
-        return {
-            pdf: 'https://yuanyulinyi.github.io/docs/设计模式.pdf' // 设置文档地址
+    setup() {
+        let input = ref('')
+        const pdfUrl = ref('')
+        const updatePdf = () => {
+            pdfUrl.value = 'https://yuanyulinyi.github.io/docs/' + encodeURIComponent(input.value) + '.pdf'
         }
-    },
-    methods: {
-        renderedHandler() {
+
+        const renderedHandler = () => {
             console.log("渲染完成")
-        },
-        errorHandler() {
+        }
+
+        const errorHandler = () => {
             console.log("渲染失败")
+            ElMessage({
+                message: '渲染失败,请检查输入路径',
+                type: 'error',
+                plain: true,
+            })
+        }
+
+        return {
+            input,
+            pdfUrl,
+            updatePdf,
+            renderedHandler,
+            errorHandler
         }
     }
 }
